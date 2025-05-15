bcbd7c8ba65c9bbca953580d3db9dfb62d3386fe
kashif-ibrar-dev <20021676@students.koi.edu.au>
2025-05-14 18:56:20 -0700

    Fix styles

--- lib/widgets/condition_card.dart
+++ lib/widgets/condition_card.dart
@@ -14,17 +14,29 @@ class ConditionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Color backgroundColor = isSelected ? Colors.teal[400]! : Colors.teal[200]!;
+    Color backgroundColor =
+        isSelected
+            ? const Color.fromRGBO(78, 129, 112, 0.4)
+            : const Color.fromRGBO(255, 255, 255, 0.65);
+    ;
 
     return GestureDetector(
       onTap: onTap,
       child: Container(
         width: 150,
-        height: 50,
+        // Removed fixed height to allow padding to expand the container
+        padding: const EdgeInsets.symmetric(vertical: 37, horizontal: 24),
         margin: const EdgeInsets.all(8),
         decoration: BoxDecoration(
           color: backgroundColor,
           borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color:
+                isSelected
+                    ? Colors.white
+                    : const Color.fromRGBO(78, 129, 112, 1),
+            width: 2,
+          ),
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.2),
@@ -33,29 +45,28 @@ class ConditionCard extends StatelessWidget {
             ),
           ],
         ),
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              if (isSelected)
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Icon(
-                    Icons.check_circle,
-                    color: Colors.white,
-                    size: 24,
-                  ),
-                ),
-              Expanded(
-                child: Center(
-                  child: Text(
-                    condition,
-                    style: const TextStyle(color: Colors.white, fontSize: 16),
-                  ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 5,
+                ), // Adjust to avoid overlap with icon
+                child: Text(
+                  condition,
+                  style: const TextStyle(color: Colors.white, fontSize: 16),
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ],
-          ),
+            ),
+            if (isSelected)
+              Positioned(
+                top: -25, // Adjusted from -36 to -20 to make the icon visible
+                right: -13,
+                child: Icon(Icons.check_circle, color: Colors.white, size: 24),
+              )
+          ],
         ),
       ),
     );
